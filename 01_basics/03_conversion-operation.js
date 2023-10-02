8e7a6347b2941e5cdea6800e71a76eeacff4433f
SmitKathiriya <110350887+SmitKathiriya7@users.noreply.github.com>
2023-10-02 10:00:37 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion-operation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+let score1 = "33abc"
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+let isloggedin = 1
+let booleanloggedin = Boolean(isloggedin)
+console.log(booleanloggedin)
+
+// 1 => true   0 => false
+// "" => false
+// "smit" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
