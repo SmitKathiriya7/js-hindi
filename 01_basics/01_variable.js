22816660b9a15211fdad203f96471b945e9d9856
SmitKathiriya <110350887+SmitKathiriya7@users.noreply.github.com>
2023-10-02 09:38:11 +0000

    Datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "smit@google.com"
+var accountPassword = "12345"
+ accountcity = "surat"
+let accountstate
+
+// accountId = 2
+accountEmail = "sk@23.com"
+accountPassword3 = "57845"
+accountcity = "mumbai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+let age = 18
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
